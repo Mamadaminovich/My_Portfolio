d29c64d0db133ebacdbdec53a5be3e60b01c2b6f
Abdulaziz <99309876+Mamadaminovich@users.noreply.github.com>
2024-04-28 08:23:12 +0500

    pp

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import get_connection
+
+def home(request):
+    return render(request, "index.html", {})
+
+@csrf_exempt
+def send_email(request):
+    if request.method == "POST":
+        subject = request.POST.get("subject")
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [request.POST.get("email"), ]
+        message = request.POST.get("message")
+
+        try:
+            with get_connection(
+                host=settings.EMAIL_HOST,
+                port=settings.EMAIL_PORT,
+                username=settings.EMAIL_HOST_USER,
+                password=settings.EMAIL_HOST_PASSWORD,
+                use_tls=settings.EMAIL_USE_TLS
+            ) as connection:
+                email = EmailMessage(subject, message, email_from, recipient_list, connection=connection)
+                email.send()
+
+                return JsonResponse({'success': True})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
+
+    return JsonResponse({'success': False, 'error': 'Method not allowed'}, status=405)
